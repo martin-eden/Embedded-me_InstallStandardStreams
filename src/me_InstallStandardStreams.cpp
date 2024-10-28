99eacbd0d94ca22164b37c4602d177c1035dce61
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-28 21:47:30 +0200

    [~/] Switch to [me_Uart] from [HardwareSerial].

--- src/me_InstallStandardStreams.cpp
+++ src/me_InstallStandardStreams.cpp
@@ -2,53 +2,14 @@
 
 /*
   Wrapped by: Martin Eden
-  Last mod.: 2024-09-12
+  Last mod.: 2024-10-27
 */
 
 #include "me_InstallStandardStreams.h"
 
-#include <Arduino.h> // fdevopen(), FILE, stdin..
-
 #include <me_BaseTypes.h>
-
-// Forwards:
-TSint_2 WriteChar(TChar Char, FILE * File);
-TSint_2 ReadChar(FILE * File);
-
-/*
-  Assign standard streams stdin, stdout, stderr to Serial.
-
-  Expected to be called once.
-
-  Based on Arduino's forum answer
-
-    https://forum.arduino.cc/t/printf-on-arduino/888528/5
-*/
-void InstallStandardStreams()
-{
-  FILE * SerialStream = fdevopen(&WriteChar, &ReadChar);
-  /*
-    fdevopen() creates stream and has side effects.
-
-    Side effects are setting std streams to newly created stream
-    (if they were not set).
-
-    We are calling it for it's side effects but still assign
-    stream to std's just in case they were set.
-
-    Documentation found at
-
-      https://onlinedocs.microchip.com/pr/GUID-317042D4-BCCE-4065-BB05-AC4312DBC2C4-en-US-2/index.html?GUID-9EF470A9-BEB2-4014-8FC3-5B39F3F86DAF
-
-      https://www.nongnu.org/avr-libc/user-manual/group__avr__stdio.html#gab599ddf60819df4cc993c724a83cb1a4
-
-    (I just searched for "C fdevopen".)
-  */
-
-  stdin = SerialStream;
-  stdout = SerialStream;
-  stderr = SerialStream;
-}
+#include <stdio.h> // fdevopen(), FILE, stdin..
+#include <me_Uart.h>
 
 /*
   Write char
@@ -60,12 +21,14 @@ void InstallStandardStreams()
     and shall return 0 if the output was successful, and a nonzero
     value if the character could not be sent to the device.
 */
-TSint_2 WriteChar(TChar Char, FILE * File __attribute__((unused)))
+TSint_2 WriteChar(
+  TChar Char,
+  FILE * File __attribute__((unused))
+)
 {
-  if (Serial.write(Char) != 1)
-    return 1;
+  me_Uart::SendByte((TUint_1) Char);
 
-  return 0;
+  return 1;
 }
 
 /*
@@ -81,19 +44,62 @@ TSint_2 WriteChar(TChar Char, FILE * File __attribute__((unused)))
     return _FDEV_ERR. If an end-of-file condition was reached while
     reading from the device, _FDEV_EOF shall be returned.
 */
-TSint_2 ReadChar(FILE * File __attribute__((unused)))
+TSint_2 ReadChar(
+  FILE * File __attribute__((unused))
+)
 {
   /*
-    Serial.read() returns -1 when stream is empty.
-    (_FDEV_ERR == -1), (_FDEV_EOF == -2). So it should work.
-
-    I hadn't test stdin functionality. This function is for completeness
-    but implementation may be changed if it does not work.
+    Unlike WriteChar() this functionality is not tested.
+    I never used stdlib for input.
   */
-  return Serial.read();
+
+  TUint_1 Value;
+
+  const TUint_2 AwaitTime_ms = 15;
+
+  if (!me_Uart::AwaitByte(&Value, AwaitTime_ms))
+    return _FDEV_ERR;
+
+  return (TSint_2) Value;
+}
+
+/*
+  Assign standard streams stdin, stdout, stderr to Serial.
+
+  Expected to be called once.
+
+  Based on Arduino's forum answer
+
+    https://forum.arduino.cc/t/printf-on-arduino/888528/5
+*/
+void InstallStandardStreams()
+{
+  FILE * Stream = fdevopen(&WriteChar, &ReadChar);
+  /*
+    fdevopen() creates stream and has side effects.
+
+    Side effects are setting std streams to newly created stream
+    (if they were not set).
+
+    We are calling it for it's side effects but still assign
+    stream to std's just in case they were set.
+
+    Documentation found at
+
+      https://onlinedocs.microchip.com/pr/GUID-317042D4-BCCE-4065-BB05-AC4312DBC2C4-en-US-2/index.html?GUID-9EF470A9-BEB2-4014-8FC3-5B39F3F86DAF
+
+      https://www.nongnu.org/avr-libc/user-manual/group__avr__stdio.html#gab599ddf60819df4cc993c724a83cb1a4
+
+    (I searched for "C fdevopen".)
+  */
+
+  stdin = Stream;
+  stdout = Stream;
+  stderr = Stream;
 }
 
 /*
   2023-11-05
   2024-05-09
+  2024-10-27 [~] Switch to [me_Uart]. Goodbye [HardwareSerial]!
 */
